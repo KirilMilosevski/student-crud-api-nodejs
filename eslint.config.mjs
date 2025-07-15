0240a8445885ef159626d2ec4650f4bba2737e5a
kirilsrepo <kirilsrepo.yahoo.com>
2025-07-15 17:39:09 +0200

    5

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,14 @@
-// eslint.config.js
-import eslintRecommended from "eslint/configs/recommended";
-
 export default [
-  eslintRecommended
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "module",
+    },
+    rules: {
+      // Add any custom rules here
+      "no-unused-vars": "warn",
+      "no-console": "off",
+    }
+  }
 ];
